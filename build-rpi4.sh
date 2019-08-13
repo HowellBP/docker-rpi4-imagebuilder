2e9e7979393f29950964be444f8718d14a8a86d9
Ben Howell <howellbp@gmail.com>
2019-08-13 15:28:26 -0500

    More debugging

--- build-rpi4.sh
+++ build-rpi4.sh
@@ -5,7 +5,7 @@
 # mounted as docker volumes to allow files to be exchanged between the host and 
 # the container.
 
-kernel_branch=rpi-4.19.y
+#kernel_branch=rpi-4.19.y
 #kernelgitrepo="https://github.com/raspberrypi/linux.git"
 #branch=bcm2711-initial-v5.2
 #kernelgitrepo="https://github.com/lategoodbye/rpi-zero.git"
